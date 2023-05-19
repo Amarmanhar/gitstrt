e2df6ae203ec4a0d3a58c2169e81e3d706b8efd7
Amarmanhar <nkamar1412@gmail.com>
2023-05-19 16:21:39 +0530

    new

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const headertitle = document.getElementById('header-title');
-const header = document.getElementById('main-header');
-const add = document.getElementById('additem');
+// const headertitle = document.getElementById('header-title');
+// const header = document.getElementById('main-header');
+// const add = document.getElementById('additem');
 
-add.style.fontWeight = '1000';
-add.style.color = "#00FF00"; 
-header.style.borderBottom = "solid 3px #000";
+// add.style.fontWeight = '1000';
+// add.style.color = "#00FF00"; 
+// header.style.borderBottom = "solid 3px #000";
+
+var item = document.getElementsByClassName('list-group-item');
+item[0].style.fontWeight ="bold";
+item[1].style.fontWeight ="bold";
+item[2].style.fontWeight ="bold";
+item[3].style.fontWeight ="bold";
+
+item[1].style.backgroundColor = "green";
