8f9d0723de3c418dc64254b4a5bbbc42c1c86a4a
Amarmanhar <nkamar1412@gmail.com>
2023-05-19 15:53:26 +0530

    new changes

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-console.log('i love to code');
-console.log("hello github i am beginer");
+const headertitle = document.getElementById('header-title');
+const header = document.getElementById('main-header');
+const add = document.getElementById('additem');
+
+add.style.fontWeight = '1000';
+add.style.color = "#00FF00"; 
+header.style.borderBottom = "solid 3px #000";
