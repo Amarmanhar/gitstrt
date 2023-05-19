527ddc12f9645336e6090c60b3539aeeb937dfb8
Amarmanhar <nkamar1412@gmail.com>
2023-05-19 17:45:08 +0530

    new

--- app.js
+++ app.js
@@ -6,10 +6,13 @@
 // add.style.color = "#00FF00"; 
 // header.style.borderBottom = "solid 3px #000";
 
-var item = document.getElementsByClassName('list-group-item');
-item[0].style.fontWeight ="bold";
-item[1].style.fontWeight ="bold";
-item[2].style.fontWeight ="bold";
-item[3].style.fontWeight ="bold";
+// var item = document.getElementsByClassName('list-group-item');
+// item[0].style.fontWeight ="bold";
+// item[1].style.fontWeight ="bold";
+// item[2].style.fontWeight ="bold";
+// item[3].style.fontWeight ="bold";
 
-item[1].style.backgroundColor = "green";
+// item[1].style.backgroundColor = "green";
+
+var submit = document.querySelector('input[type = "submit"]');
+submit.value = "send";
