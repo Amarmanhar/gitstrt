8023c15c5fae0a2455cecb3994aaaba333104900
Amarmanhar <nkamar1412@gmail.com>
2023-05-26 11:46:42 +0530

    newww

--- main_1.js
+++ main_1.js
@@ -1,13 +1,43 @@
+// function saveToLocalStorage(event){
+//     event.preventDefault();
+
+//     const fname = event.target.fname.value;
+//     const lname = event.target.lname.value;
+//     const email = event.target.email.value;
+//     const password = event.target.password.value;
+
+//     localStorage.setItem('firstname', fname);
+//     localStorage.setItem('lastname', lname);
+//     localStorage.setItem('emailId', email);
+//     localStorage.setItem('password', password);
+// 
+
+// Retrieve the registration form element
+//const form = document.getElementById('register');
+
+//  save to local storage 
 function saveToLocalStorage(event){
-    event.preventDefault();
+   event.preventDefault();
 
-    const fname = event.target.fname.value;
-    const lname = event.target.lname.value;
-    const email = event.target.email.value;
-    const password = event.target.password.value;
+   const fname = event.target.fname.value;
+   const lname = event.target.lname.value;
+   const email = event.target.email.value;
+   const password = event.target.password.value;
 
-    localStorage.setItem('firstname', fname);
-    localStorage.setItem('lastname', lname);
-    localStorage.setItem('emailId', email);
-    localStorage.setItem('password', password);
+   const obj = {
+      fname: fname,
+      lname: lname,
+      email: email,
+      password: password
+   }
+   localStorage.setItem('userdetails', JSON.stringify(obj));
+    showOnscreen(obj);
+}
+function showOnscreen(obj){
+   const parentEle = document.getElementById('listofitems');
+   parentEle.innerHTML = parentEle.innerHTML + `<li> ${obj.fname} - ${obj.lname} - ${obj.email} - ${obj.password}`;
+
+   // const childele = document.createElement('li');
+   // childele.textContent = obj.fname + '-' + obj.lname + "-" + obj.email + "-" + obj.password;
+   // parentEle.append(childele);
 }
